5e99380c4cd476c34c80152a5b84992a5c1babec
Agyn <agyn.bolatov.1992@gmail.com>
2025-01-14 19:27:12 +0500

    add one more field to items

--- routes/items.js
+++ routes/items.js
@@ -3,53 +3,52 @@ const express = require('express');
 module.exports = (pool) => {
   const router = express.Router();
 
-  // Create item
+  // Create items
   router.post('/', async (req, res) => {
     const { items } = req.body;
-  
+
     if (!Array.isArray(items) || items.length === 0) {
       return res.status(400).json({ error: 'Invalid data: items array is required.' });
     }
-  
+
     try {
       const client = await pool.connect();
-  
+
       // Truncate the 'items' table before inserting new items
       await client.query('TRUNCATE TABLE items RESTART IDENTITY CASCADE');
-      
+
       // Create a list of values for the batch insert
       const values = [];
       items.forEach((item, index) => {
-        const { name, price, balance } = item;
-        values.push(`($${index * 3 + 1}, $${index * 3 + 2}, $${index * 3 + 3})`);
+        values.push(
+          `($${index * 4 + 1}, $${index * 4 + 2}, $${index * 4 + 3}, $${index * 4 + 4})`
+        );
       });
-  
+
       // Create a query to insert all items at once
       const query = `
-        INSERT INTO items (name, price, balance) 
+        INSERT INTO items (name, price, balance, vidprice) 
         VALUES ${values.join(', ')} 
         RETURNING *;
       `;
-      
+
       // Flatten the values array to pass as query parameters
       const flattenedValues = items.reduce((acc, item) => {
-        const { name, price, balance } = item;
-        acc.push(name, price, balance);
+        acc.push(item.name, item.price, item.balance, item.vidprice);
         return acc;
       }, []);
-      
+
       // Execute the batch insert query
       const result = await client.query(query, flattenedValues);
       client.release();
-  
+
       // Return the inserted items
       res.json({ items: result.rows });
     } catch (err) {
       res.status(500).json({ error: err.message });
     }
   });
-  
-  
+
   // Get all items
   router.get('/', async (req, res) => {
     try {
@@ -62,5 +61,3 @@ module.exports = (pool) => {
 
   return router;
 };
-
-
